76e25ff1d9fb858511a15b1a2d73d67191ed2dce
venky4c <venky4c@gmail.com>
2020-07-14 16:01:53 +0530

    Delte buttons in Search bar. WIP

--- dist/src/components/SearchBar.js
+++ dist/src/components/SearchBar.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Tag from "./Tag";
 
 function SearchBar(props) {
+  //const [filterValues, setFilterValues] = useState[filterValues]
+
+  const filterValues = props.filterValues;
+  console.log(`props.filterValues ${filterValues}`);
+  const [tags, setTags] = useState([...filterValues]);
+  const onDelete = (index) => {
+    console.log(`tags before ${tags.length}`);
+    tags.splice(index, 1);
+    console.log(`tags after ${tags.length}`);
+    setTags(tags);
+    console.log(`index value is ${index}`);
+    //console.log(`the filtered values from SearchBar are ${tags}`);
+    //setFilterValues(filterValues);
+  };
+
+  //console.log(`props.filterValues ${filterValues}`);
   return (
     <div className="search-bar">
       <div>
-        {props.filterValues.map((value) => (
-          <Tag name={value} />
+        {filterValues.map((value, index) => (
+          <Tag
+            id={index}
+            buttonName={value}
+            tags={tags}
+            index={index}
+            deleteButton={() => onDelete(index)}
+          />
         ))}
       </div>
     </div>
--- dist/src/components/Tag.js
+++ dist/src/components/Tag.js
@@ -1,37 +1,34 @@
 import React, { useState } from "react";
-// const [tagName, setTagName] = useState([]);
-// const removeButton = (e) => {
-//   //e.preventDefault();
-//   setTagName((prevState) => [...prevState, tagName]);
-//   console.log("reached");
-//   var elem = document.getElementById("btn");
-//   elem.parentNode.remove();
-//   return false;
-// };
+
 function Tag(props) {
-  const [tagName, setTagName] = useState([]);
-  const removeButton = (e) => {
-    //e.preventDefault();
-    setTagName((prevState) => [...prevState, tagName]);
-    console.log("reached");
-    var elem = document.getElementById("btn");
-    elem.parentNode.remove();
-    return false;
+  const { tags, buttonName, deleteButton, index } = props;
+  //const [tagName, setTagName] = useState([buttonName]);
+  const _delete = (e) => {
+    e.preventDefault();
+    deleteButton(index);
   };
+  console.log(`you want to filter by  ${buttonName}`);
   return (
     // <form action="">
     <div id="search-tags">
-      <span className="tag" name="tagName" multiple="true">
-        {props.name}
+      <span className="tag" name="buttonName" id="btn" multiple="true">
+        {buttonName}
         <button
           className="delete is-small is-primary"
-          id="btn"
-          onClick={removeButton}
+          onClick={(e) => _delete(e)}
         ></button>
       </span>
     </div>
     // </form>
   );
 }
-
 export default Tag;
+// const deleteButton = (e) => {
+//   e.preventDefault();
+//   setTagName((prevState) => [...prevState, buttonName]);
+//   console.log(`tagName is ${tagName}`);
+//   var elem = document.getElementById("btn");
+//   console.log(`elem is >>>>>> ${elem}`);
+//   elem.remove();
+//   return false;
+// };
